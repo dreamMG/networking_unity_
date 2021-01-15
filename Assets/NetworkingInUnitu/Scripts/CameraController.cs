e1e98b3bb4f017ecaa78735811a48d5f2ea505f5
gabrys-at-806959355251 <michal-gabrys98@wp.pl>
2021-01-15 13:43:29 +0100

    Initial commit

---
+++ Assets/NetworkingInUnitu/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System;
+using UnityEngine;
+
+public class CameraController : NetworkBehaviour
+{
+    private Transform target;
+    public Transform Target { set => target = value; }
+    [SerializeField] private Vector3 offset = Vector3.zero;
+    [SerializeField] private float speedFollow = 4f;
+
+    [Client]
+    private void LateUpdate() => FollowTarget();
+
+    private void FollowTarget()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, speedFollow * Time.deltaTime);
+    }
+}
---
+++ Assets/NetworkingInUnitu/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] CharacterController controller = null;
+
+    Vector3 movement;
+
+    public override void OnStartAuthority()
+    {
+        enabled = true;
+        FindObjectOfType<CameraController>().Target = transform;
+    }
+
+    [ClientCallback]
+    private void Update() => Move();
+
+    [Client]
+    private void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.CompareTag("Ground"))
+                    movement = hit.point;
+            }
+        }
+
+        controller.Move((movement - transform.position) * moveSpeed * Time.deltaTime);
+    }
+}
